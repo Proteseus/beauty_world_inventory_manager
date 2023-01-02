997f407020358e86b94dbf402c0ac8e4a47867b6
Lewi Belayneh Haile <horriblebob11@gmail.com>
2023-01-02 22:43:10 +0300

    file refactoring for readability

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import data_fetch as dd
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     data = dd.fetchAll()
@@ -10,16 +11,19 @@ def index():
         data[i]['Bar Code'] = int(data[i]['Bar Code'])
     return render_template('index.html', data=data, len=len(data))
 
-@app.route('/edit/<id>', methods=['GET'])
-def edit():
+
+@app.route('/edit/<item_name>', methods=['GET'])
+def edit(item_name: str):
     pass
 
-@app.route('/delete/<id>', methods=['GET'])
-def delete(id):
-    print(id)
-    print(type(id))
-    dd.delete_item(id)
+
+@app.route('/delete/<item_name>', methods=['GET'])
+def delete(item_name: str):
+    print(item_name)
+    print(type(item_name))
+    dd.delete_item(item_name)
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
